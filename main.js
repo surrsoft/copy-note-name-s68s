afb3072632baa52e1c4639e4b58a5b93a50493a0
surr <rybinsk.ru@gmail.com>
2025-01-25 13:05:53 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const { Plugin, Notice } = require("obsidian");
+
+module.exports = class CopyNoteNamePlugin extends Plugin {
+  async onload() {
+    // Добавляем иконку на боковую панель
+    this.addRibbonIcon("copy", "Скопировать имя заметки", () => {
+      // Получаем активную заметку
+      const activeFile = this.app.workspace.getActiveFile();
+      if (activeFile) {
+        const noteName = activeFile.basename;
+        // Копируем имя заметки в буфер обмена
+        navigator.clipboard
+          .writeText(noteName)
+          .then(() => {
+            new Notice("Имя заметки скопировано в буфер обмена");
+          })
+          .catch((err) => {
+            new Notice("Ошибка при копировании: " + err);
+          });
+      } else {
+        new Notice("Нет активной заметки");
+      }
+    });
+  }
+
+  onunload() {
+    // Очистка при отключении плагина (если необходимо)
+  }
+};
